fa0b53ca9285cc782ca20427ef97c6fec573b374
manityatyagi19 <manityatyagi19@gmail.com>
2026-01-28 00:32:48 +0530

    changes 3

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// eslint.config.js
+import { FlatCompat } from "@eslint/eslintrc";
+const compat = new FlatCompat({
+  baseDirectory: process.cwd(),
+});
+
+export default [
+  ...compat.extends("eslint:recommended"),
+  {
+    files: ["*.js"],
+    rules: {
+      // Example rules
+      "no-console": "warn",
+      "semi": ["error", "always"],
+    },
+  },
+];
