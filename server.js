2e922d7e0e2b09d9c561b5cc12bc13ddaa994cf9
manityatyagi19 <manityatyagi19@gmail.com>
2026-01-29 22:24:59 +0530

    Docker compose file added

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 import "dotenv/config";
 import express from "express";
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
     return res.json({
         success: true,
         message: "Github Actions X Nodejs"
-    })
-})
+    });
+});
 
 app.get("/heath", (req, res) => {
     return res.json({
         success: true,
         message: "Server is healthy"
-    })
-})
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
-})
+});
 
